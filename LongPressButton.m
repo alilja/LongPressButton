6070e3cabb448e47137d1ecb187cca393b2c3fc4
Andrew Lilja <andrewlilja@gmail.com>
2014-08-03 21:01:13 -0500

    added license and minimum value

--- LongPressButton.m
+++ LongPressButton.m
@@ -2,9 +2,26 @@
 //  LongPressButton.m
 //  LiftingTracker
 //
-//  Created by Andrew on 7/21/14.
-//  Copyright (c) 2014 Andrew Lilja. All rights reserved.
 //
+// Copyright (c) 2014 Andrew Lilja
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #import "LongPressButton.h"
 
@@ -18,6 +35,7 @@
         self.defaultColor = [UIColor blueColor];
         self.matchedValueColor = [UIColor redColor];
         self.active = true;
+        self.minimumValue = nil;
         [self setTitle:[@(self.value) stringValue] forState:UIControlStateNormal];
         [self setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     }
@@ -33,8 +51,8 @@
         int delta = round((verticalCenter)/self.scalingValue);
         int tempDisplay = self.value - delta;
         
-        if(tempDisplay < 0){
-            tempDisplay = 0;
+        if(self.minimumValue != nil && tempDisplay < [self.minimumValue intValue]){
+            tempDisplay = [self.minimumValue intValue];
         }
         switch(sender.state){
             case UIGestureRecognizerStateEnded:
